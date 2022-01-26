5a70723a2840611f05c0d54e81bda90069b39a18
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-01-26 14:46:45 +0100

    modifier contenu de page realisations

--- content/product_content.php
+++ content/product_content.php
@@ -5,14 +5,14 @@
                     <div class="product-item-title d-flex">
                         <div class="bg-faded p-5 d-flex ms-auto rounded">
                             <h2 class="section-heading mb-0">
-                                <span class="section-heading-upper">Blended to Perfection</span>
-                                <span class="section-heading-lower">Coffees & Teas</span>
+                                <span class="section-heading-upper">PATISSERIE</span>
+                                <span class="section-heading-lower">NOS MEILLEURS INGRÉDIENTS</span>
                             </h2>
                         </div>
                     </div>
                     <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="assets/img/products-01.jpg" alt="..." />
                     <div class="product-item-description d-flex me-auto">
-                        <div class="bg-faded p-5 rounded"><p class="mb-0">We take pride in our work, and it shows. Every time you order a beverage from us, we guarantee that it will be an experience worth having. Whether it's our world famous Venezuelan Cappuccino, a refreshing iced herbal tea, or something as simple as a cup of speciality sourced black coffee, you will be coming back for more.</p></div>
+                        <div class="bg-faded p-5 rounded"><p class="mb-0">Nous sommes fier de nos produits. Du chocolat aux produits crémiers en passant par les pépites de décoration, tout est disponible dans nos boutiques.</p></div>
                     </div>
                 </div>
             </div>
@@ -23,14 +23,14 @@
                     <div class="product-item-title d-flex">
                         <div class="bg-faded p-5 d-flex me-auto rounded">
                             <h2 class="section-heading mb-0">
-                                <span class="section-heading-upper">Delicious Treats, Good Eats</span>
-                                <span class="section-heading-lower">Bakery & Kitchen</span>
+                                <span class="section-heading-upper">EQUIPEZ VOUS COMME IL FAUT</span>
+                                <span class="section-heading-lower">USTENSILES</span>
                             </h2>
                         </div>
                     </div>
                     <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="assets/img/products-02.jpg" alt="..." />
                     <div class="product-item-description d-flex ms-auto">
-                        <div class="bg-faded p-5 rounded"><p class="mb-0">Our seasonal menu features delicious snacks, baked goods, and even full meals perfect for breakfast or lunchtime. We source our ingredients from local, oragnic farms whenever possible, alongside premium vendors for specialty goods.</p></div>
+                        <div class="bg-faded p-5 rounded"><p class="mb-0">Vous pouvez choisir de travailler dans les meilleurs conditions, en achetant nos ustensiles Nesti. Beaux et fiables, vous pouvez les offrir à vos proches, et ainsi être sur de leur faire plaisir</p></div>
                     </div>
                 </div>
             </div>
@@ -41,14 +41,14 @@
                     <div class="product-item-title d-flex">
                         <div class="bg-faded p-5 d-flex ms-auto rounded">
                             <h2 class="section-heading mb-0">
-                                <span class="section-heading-upper">From Around the World</span>
-                                <span class="section-heading-lower">Bulk Speciality Blends</span>
+                                <span class="section-heading-upper">EN TOUTE SAISON</span>
+                                <span class="section-heading-lower">NOS RECETTES</span>
                             </h2>
                         </div>
                     </div>
                     <img class="product-item-img mx-auto d-flex rounded img-fluid mb-3 mb-lg-0" src="assets/img/products-03.jpg" alt="..." />
                     <div class="product-item-description d-flex me-auto">
-                        <div class="bg-faded p-5 rounded"><p class="mb-0">Travelling the world for the very best quality coffee is something take pride in. When you visit us, you'll always find new blends from around the world, mainly from regions in Central and South America. We sell our blends in smaller to large bulk quantities. Please visit us in person for more details.</p></div>
+                        <div class="bg-faded p-5 rounded"><p class="mb-0">Cherchez l'inspiration parmis la multitude de recettes testées et approuvées par nos colaborateurs</p></div>
                     </div>
                 </div>
             </div>
