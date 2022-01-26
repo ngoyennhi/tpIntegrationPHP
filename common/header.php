12abc36a2cd4b0a9c775c0b6a0b6713367c1ed1b
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-01-26 10:31:49 +0100

    déménager sur MacOS

---
+++ common/header.php
@@ -0,0 +1,6 @@
+<header>
+<h1 class="site-heading text-center text-white d-none d-lg-block"​>
+<span class="site-heading-upper text-primary mb-3">​Projet Fil Rouge​</span>
+<span class="​site-heading-lower">​Nesti​</span>
+</h1>
+</header>
