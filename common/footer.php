0e7cfdaa03e9d35f757b4d999c2a9aa03d3eda85
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-01-26 12:46:32 +0100

    Update footer.php

--- common/footer.php
+++ common/footer.php
@@ -1,3 +1,3 @@
 <footer class="footer text-faded text-center py-5">
-<div class="container"><p class="m-0 small">Copyright &copy; Your Website 2021</p></div>
+<div class="container"><p class="m-0 small">Copyright &copy; Needemand-Pedago 2020</p></div>
 </footer>
