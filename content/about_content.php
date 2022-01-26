981013a16f57444370d65bbb1baf72e41b5ced6b
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-01-26 14:17:14 +0100

    change color text footer.php

--- content/about_content.php
+++ content/about_content.php
@@ -1,19 +1,21 @@
 <section class="page-section about-heading">
 <div class="container">
+<!-- bootrap class = "mx-auto d-block" pour mettre au centre -->
 <img class="img-fluid rounded about-heading-img mb-3 mb-lg-0 col-xl-6 col-lg-8 w-60 h-60 mx-auto d-block" src="assets/img/about.jpg" alt="..." />                
                 <div class="about-heading-content">
                     <div class="row">
                         <div class="col-xl-9 col-lg-10 mx-auto">
                             <div class="bg-faded rounded p-5">
                                 <h2 class="section-heading mb-4">
-                                    <span class="section-heading-upper">Strong Coffee, Strong Roots</span>
-                                    <span class="section-heading-lower">About Our Cafe</span>
+                                    <span class="section-heading-upper">ENTREPRISE FICTIVE À BUT PÉDAGOGIQUE</span>
+                                    <span class="section-heading-lower">ABOUT NESTI</span>
                                 </h2>
-                                <p>Founded in 1987 by the Hernandez brothers, our establishment has been serving up rich coffee sourced from artisan farmers in various regions of South and Central America. We are dedicated to travelling the world, finding the best coffee, and bringing back to you here in our cafe.</p>
+                                <p>Fondé en 1984 par les Frères Hernandez, notre établissement a fourni un excelent chocolat venant des quatres coins du monde. C'est en 1945, que Marina Hernadez, decide de diversifier l'entreprise familiale, sans perdre en prestige. <br>
+                                Nesti devint alors une référence dans le monde de la <em>patisserie</em> <br>
+                                Nous consacrons notre temps à trouver les meilleurs ingrédients à travers le monde.
+                                </p>
                                 <p class="mb-0">
-                                    We guarantee that you will fall in
-                                    <em>lust</em>
-                                    with our decadent blends the moment you walk inside until you finish your last sip. Join us for your daily routine, an outing with friends, or simply just to enjoy some alone time.
+                                Afin que vous puissiez <em>en famille </em>et entre amis continuer à perpetuer l'art de la patisserie. Parce que c'est toujours un plaisir de partager !
                                 </p>
                             </div>
                         </div>
