e146e117e1c8d54d21f326de48d99744bdeffff4
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-01-26 12:43:51 +0100

    updated page home

--- content/index_content.php
+++ content/index_content.php
@@ -1,14 +1,14 @@
 <section class="page-section clearfix">
             <div class="container">
                 <div class="intro">
-                    <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="content/page1.jpeg" alt="..." />
+                    <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="content/intro.jpeg" alt="..." />
                     <div class="intro-text left-0 text-center bg-faded p-5 rounded">
                         <h2 class="section-heading mb-4">
-                            <span class="section-heading-upper">Fresh Coffee</span>
-                            <span class="section-heading-lower">Worth Drinking</span>
+                            <span class="section-heading-upper">NESTI</span>
+                            <span class="section-heading-lower">PATISSERIE</span>
                         </h2>
-                        <p class="mb-3">Every cup of our quality artisan coffee starts with locally sourced, hand picked ingredients. Once you try it, our coffee will be a blissful addition to your everyday morning routine - we guarantee it!</p>
-                        <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl" href="#!">Visit Us Today!</a></div>
+                        <p class="mb-3">Le plaisir de partager</p>
+                        <div class="intro-button mx-auto"><a class="btn btn-primary btn-xl text-light " href="#!">Actualités</a></div>
                     </div>
                 </div>
             </div>
@@ -19,10 +19,10 @@
                     <div class="col-xl-9 mx-auto">
                         <div class="cta-inner bg-faded text-center rounded">
                             <h2 class="section-heading mb-4">
-                                <span class="section-heading-upper">Our Promise</span>
-                                <span class="section-heading-lower">To You</span>
+                                <span class="section-heading-upper">CHOCOLATIER DEPUIS</span>
+                                <span class="section-heading-lower">1884</span>
                             </h2>
-                            <p class="mb-0">When you walk into our shop to start your day, we are dedicated to providing you with friendly service, a welcoming atmosphere, and above all else, excellent products made with the highest quality ingredients. If you are not satisfied, please let us know and we will do whatever we can to make things right!</p>
+                            <p class="mb-0">Fort de notre expérience aujourd'hui nous vous offrons les meilleurs moments de dégustation</p>
                         </div>
                     </div>
                 </div>
