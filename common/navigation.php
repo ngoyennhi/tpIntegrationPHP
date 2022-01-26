8516e23b5123d792388dccffceb90fcdcf3597af
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-01-26 22:23:21 +0100

    modififer CSS Navbar Home active

--- common/navigation.php
+++ common/navigation.php
@@ -4,10 +4,10 @@
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mx-auto">
-                        <li class="nav-item <?= ($loc=='home')?'active':''; ?> px-lg-4"><a class="nav-link text-uppercase" href="index.php?loc=home">HOME</a></li>
-                        <li class="nav-item <?= ($loc =='about')?'active':''; ?> px-lg-4"><a class="nav-link text-uppercase" href="index.php?loc=about">ABOUT</a></li>
-                        <li class="nav-item <?= ($loc == 'products')?'active':''; ?> px-lg-4"><a class="nav-link text-uppercase" href="index.php?loc=products">PRODUCTS</a></li>
-                        <li class="nav-item <?= ($loc == 'realisations')?'active':''; ?> px-lg-4"><a class="nav-link text-uppercase" href="index.php?loc=realisations">REALISATIONS</a></li>
+                        <li class="nav-item active px-lg-4"><a class="nav-link text-uppercase" href="index.php?loc=home">HOME</a></li>
+                        <li class="nav-item  px-lg-4"><a class="nav-link text-uppercase" href="index.php?loc=about">ABOUT</a></li>
+                        <li class="nav-item  px-lg-4"><a class="nav-link text-uppercase" href="index.php?loc=products">PRODUCTS</a></li>
+                        <li class="nav-item  px-lg-4"><a class="nav-link text-uppercase" href="index.php?loc=realisations">REALISATIONS</a></li>
                     </ul>
                 </div>
             </div>
--- content/about_content.php
+++ content/about_content.php
@@ -1,7 +1,7 @@
 <section class="page-section about-heading">
 <div class="container">
 <!-- bootrap class = "mx-auto d-block" pour mettre au centre -->
-<img class="img-fluid rounded about-heading-img mb-3 mb-lg-0 col-xl-6 col-lg-8 w-60 h-60 mx-auto d-block" src="assets/img/about.jpg" alt="..." />                
+<img class="img-fluid rounded about-heading-img mb-3 mb-lg-0 col-xl-6 mx-auto d-block" src="assets/img/about.jpg" alt="..." />                
                 <div class="about-heading-content">
                     <div class="row">
                         <div class="col-xl-9 col-lg-10 mx-auto">
@@ -15,7 +15,7 @@
                                 Nous consacrons notre temps à trouver les meilleurs ingrédients à travers le monde.
                                 </p>
                                 <p class="mb-0">
-                                Afin que vous puissiez <em>en famille </em>et entre amis continuer à perpetuer l'art de la patisserie. Parce que c'est toujours un plaisir de partager !
+                                Afin que vous puissiez <em>en famille et entre amis </em>continuer à perpetuer l'art de la patisserie. Parce que c'est toujours un plaisir de partager !
                                 </p>
                             </div>
                         </div>
