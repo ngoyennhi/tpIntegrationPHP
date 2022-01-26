3ee44ae5c35e66637775aa3d99a5ead485e3a657
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-01-26 14:50:35 +0100

    placer image dans la repertoire img

--- content/index_content.php
+++ content/index_content.php
@@ -1,7 +1,7 @@
 <section class="page-section clearfix">
             <div class="container">
                 <div class="intro">
-                    <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="content/intro.jpeg" alt="..." />
+                    <img class="intro-img img-fluid mb-3 mb-lg-0 rounded" src="assets/img/intro.jpeg" alt="..." />
                     <div class="intro-text left-0 text-center bg-faded p-5 rounded">
                         <h2 class="section-heading mb-4">
                             <span class="section-heading-upper">NESTI</span>
