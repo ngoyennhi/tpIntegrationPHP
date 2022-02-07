533c4ec5eaea1060ff28051b7dac6faaad3c763a
YenNhiHTM <ngoyennhi.ho@gmail.com>
2022-02-07 21:27:59 +0100

    Update connexion.php

--- control/connexion.php
+++ control/connexion.php
@@ -8,7 +8,7 @@ $dbname = "commentaire";
 
 try {
   //On se connecte a la BDD
-    $conn = new PDO("mysql:host=".$servername.";dbname=".$dbname, $username, $password);
+  $conn = new PDO("mysql:host=".$servername.";dbname=".$dbname, $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   //echo "Connected successfully";
